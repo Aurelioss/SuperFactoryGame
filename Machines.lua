642a1b62f78c7c918fd1409d6ef0fd679024df99
Aurelioss <baaurelienba@gmail.com>
2022-01-03 22:46:40 +0100

    start machine

---
+++ Machines.lua
@@ -0,0 +1,14 @@
+-- Factory
+
+M_Factory = {
+    name = "MyFactory",
+    maxMachines = 5,
+    machines = {},
+    level = 0,
+    golds = 10
+}
+
+M_Storage = {
+    capacity = 10,
+    price = 15
+}
