7104f4f3ee444489dda791141c018911c2d8b748
Aurelioss <baaurelienba@gmail.com>
2022-01-05 11:15:49 +0100

    update

--- Game.lua
+++ Game.lua
@@ -1,20 +1,40 @@
---Game: Une entreprise faut améliorer ses machines pour qu'elle devienne plus grosse
+--Game: Upgrade machines of your factory in order to grow up
 
 require("Machines")
 require("Text")
 require("Func")
 
-print(Txt_WELCOME)
-
-M_Factory.name = GetFactoryName()
+-- Variables
 
 Cont = true
 NbTurn = 0
 
+Submenu = {
+    [1] = Func_List()
+}
+
+-- End Variables
+
+
+
+-- Game start
+
+-- show welcome message + get player's name
+
+print(Txt_WELCOME)
+
+ Ru = Func_GetFactoryName()
+
+-- End game start
+
+-- Game loop
+
 while (Cont) do
     print("Turn number", NbTurn, " Golds :", M_Factory.golds, "Machines :", #M_Factory.machines, "/", M_Factory.maxMachines, "\n")
-    print(Txt_MAINMENU)
 
+    local choosingSubmenu = Func_GetChoosedFromMain()
+
+    print(Submenu[choosingSubmenu])
 
     NbTurn = NbTurn + 1
 
